e4dc9b1865860829dd609e0787a380aaceb5d4e0
dotmilk <dekathexis@gmail.com>
2020-11-14 14:35:02 -0600

    for push to bigmac

--- init.el
+++ init.el
@@ -11,7 +11,8 @@
 ;; (pallet-mode t)
 ;;(require 'benchmark-init)
 ;; To disable collection of benchmark data after init is done.
-
+(setq warning-minimum-level :emergency)
+(setf default-directory "/Users/dotmilk/")
 (defconst emacs-start-time (current-time))
 (setq gc-cons-threshold 100000000)
 (defvar milk-dir user-emacs-directory)
